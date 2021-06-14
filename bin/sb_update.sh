328efec5d31c8cf7e2aa844d7ea8854248984135
Sam Ritchie <sam.ritchie@outlook.com.au>
2021-06-14 11:43:44 +1000

    updated vimrc and added some scripts

--- bin/sb_update.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-steamcmd +login s1m1s sBatteryhorsestaple1 +force_install_dir $HOME/starbound +app_update 211820 +workshop_download_item 211820 729426797 validate +quit
-cd $HOME/starbound/mods
-rm *
-rm -r ./PropPack
-git clone https://github.com/asperrusual/PropPack.git
-$HOME/bin/mod_mover.py
