0e97dedef0fbb47a74225fdd143cfa4f32ba6d3b
Sam Ritchie <sam.ritchie@outlook.com.au>
2021-07-11 10:36:30 +1000

    Sway: added waybar toggle button, added wayland env vars

---
+++ zsh/010-wayland.zsh
@@ -0,0 +1,5 @@
+# Set up QT decorations on sway
+export QT_QPA_PLATFORM=wayland-egl
+export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
+export QT_WAYLAND_FORCE_DPI=physical
+export MOZ_ENABLE_WAYLAND=1
