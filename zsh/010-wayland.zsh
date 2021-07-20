00ea50fd9a0e54a3e5e82c0ec6d077917fdd2a06
Sam Ritchie <sam.ritchie@outlook.com.au>
2021-07-20 16:45:11 +1000

    Sway: added env variables for QT on sway

--- zsh/010-wayland.zsh
+++ zsh/010-wayland.zsh
@@ -1,5 +1,7 @@
 # Set up QT decorations on sway
-export QT_QPA_PLATFORM=wayland-egl
+export QT_SELECT=5
+export QT_STYLE_OVERRIDE=gtk2
+export QT_QPA_PLATFORM=wayland
 export QT_WAYLAND_DISABLE_WINDOWDECORATION="1"
 export QT_WAYLAND_FORCE_DPI=physical
 export MOZ_ENABLE_WAYLAND=1
