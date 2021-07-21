a900c9b72ee740a2fb462fea14452fe2ea61fce0
Sam <samm0s@hotmail.com>
2021-07-21 10:14:38 +1000

    Neovim: added shortcut for telescope cheat

--- .config/nvim/lua/config/telescope_setup.lua
+++ .config/nvim/lua/config/telescope_setup.lua
@@ -6,3 +6,4 @@ map('n', '<c-a>', [[<cmd>Telescope buffers show_all_buffers=true sort_lastused=t
 map('n', '<c-p>', [[<cmd>Telescope git_files<cr>]], silent)
 map('n', '<c-d>', [[<cmd>Telescope find_files<cr>]], silent)
 map('n', '<c-g>', [[<cmd>Telescope live_grep<cr>]], silent)
+map('n', '<leader>cs', [[<cmd>Telescope cheat fd<cr>]], silent)
