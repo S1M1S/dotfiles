19a58b446413d85fd19fff50b717d37696696fd6
Sam <samm0s@hotmail.com>
2022-07-09 08:12:01 +1000

    nvim: cosmetic updates to plugins.lua

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -9,4 +9,4 @@ vim.g.neon_italic_keyword = true
 vim.g.neon_italic_function = true
 -- vim.g.neon_transparent = true
 
-vim.cmd[[colorscheme neon]]
+vim.cmd([[colorscheme neon]])
--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -8,7 +8,8 @@ return require('packer').startup(function()
   use {
     { 'mattn/emmet-vim', keys = {{ 'i', '<C-y>' }} }, -- Emmet
     { 'rstacruz/vim-hyperstyle', ft = { 'css' } },
-    { 'ggandor/lightspeed.nvim', -- Racing around docs with s and S
+    {
+      'ggandor/lightspeed.nvim', -- Racing around docs with s and S
       keys = {
         { 'n', 's' },
         { 'n', 'S' },
@@ -16,12 +17,10 @@ return require('packer').startup(function()
         { 'n', 'T' },
         { 'n', 'f' },
         { 'n', 'F' },
-      }
+      },
     },
-  }
 
-  -- TPope plugins necessary for survival
-  use {
+    -- TPope plugins necessary for survival
     { 'tpope/vim-abolish', cmd = { 'S', 'Subvert', 'Abolish' } },
     { 'tpope/vim-commentary', keys = 'gcc', cmd = { 'Commentary' } },
     { 'tpope/vim-dispatch', cmd = { 'Dispatch', 'Make', 'Focus', 'Start' } },
@@ -33,55 +32,53 @@ return require('packer').startup(function()
     'tpope/vim-unimpaired',
     { 'tpope/vim-ragtag', ft = { 'html', 'erb' } },
     { 'tpope/vim-rails', config = [[require('config.rails')]] },
-    { 'tpope/vim-bundler' },
-  }
+    'tpope/vim-bundler',
+    'lambdalisue/suda.vim',
 
-  -- ALE for linting and fixing
-  use {
-    'w0rp/ale',
-    ft = {
-      'bash',
-      'html',
-      'javascript',
-      'json',
-      'lua',
-      'markdown',
-      'ruby',
-      'sh',
-      'vim',
-      'zsh',
+    -- ALE for linting and fixing
+    {
+      'w0rp/ale',
+      ft = {
+        'bash',
+        'html',
+        'javascript',
+        'json',
+        'lua',
+        'markdown',
+        'ruby',
+        'sh',
+        'vim',
+        'zsh',
+      },
+      config = [[vim.cmd('ALEEnable')]],
     },
-    config = 'vim.cmd[[ALEEnable]]'
-  }
 
-  -- Syntax highlighting
-  use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-  use { 'nvim-treesitter/nvim-treesitter-context' }
-  use { 'nvim-treesitter/nvim-treesitter-textobjects' }
-  use { 'rrethy/nvim-treesitter-textsubjects' }
-  require('config.treesitter')
+    -- Syntax highlighting
+    { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' },
+    'nvim-treesitter/nvim-treesitter-context',
+    'nvim-treesitter/nvim-treesitter-textobjects',
+    { 'rrethy/nvim-treesitter-textsubjects', config = [[require('config.treesitter')]] },
 
-  use { 'rafamadriz/neon', config = [[require('config.theme')]] }
+    { 'rafamadriz/neon', config = [[require('config.theme')]] },
 
-  -- LSP
-  use { 'neovim/nvim-lspconfig', }
-  use { 'williamboman/nvim-lsp-installer', }
-  use { 'onsails/lspkind-nvim' }
-  require('config.lsp')
+    -- LSP
+    { 'neovim/nvim-lspconfig', },
+    { 'williamboman/nvim-lsp-installer', },
+    { 'onsails/lspkind-nvim', config = [[require('config.lsp')]] },
 
-  -- Snippets
-  use { 'hrsh7th/nvim-cmp', }
-  use { 'hrsh7th/cmp-vsnip', requires = { 'hrsh7th/vim-vsnip' } }
-  use { 'hrsh7th/cmp-nvim-lsp' }
-  use { 'hrsh7th/cmp-buffer' }
-  use { 'hrsh7th/cmp-path' }
-  use { 'rafamadriz/friendly-snippets' }
-  require('config.cmp')
+    -- Snippets
+    'L3MON4D3/LuaSnip',
+    'saadparwaiz1/cmp_luasnip',
+    'hrsh7th/cmp-nvim-lsp',
+    'hrsh7th/cmp-buffer',
+    'hrsh7th/cmp-path',
+    'rafamadriz/friendly-snippets',
+    { 'hrsh7th/nvim-cmp', config = [[require('config.cmp')]] },
 
-  -- let's see if autopairs are any good
-  use { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end }
-  -- Git
-  use {
+    -- let's see if autopairs are any good
+    { "windwp/nvim-autopairs", config = function() require("nvim-autopairs").setup {} end },
+
+    -- Git
     { 'tpope/vim-fugitive', cmd = { 'Git', 'Gstatus', 'Gblame', 'Gpush', 'Gpull' }, disable = true },
     {
       'lewis6991/gitsigns.nvim',
@@ -90,31 +87,34 @@ return require('packer').startup(function()
       event = 'BufEnter',
     },
     { 'TimUntersberger/neogit', keys = '<leader>g', cmd = 'Neogit', config = [[require('config.neogit')]] },
-  }
 
-  -- Search
-  use {
-    'nvim-telescope/telescope.nvim',
-    requires = {
-      { 'nvim-lua/popup.nvim' },
-      { 'nvim-lua/plenary.nvim' },
+    -- Search
+    {
+      'nvim-telescope/telescope.nvim',
+      requires = {
+        { 'nvim-lua/popup.nvim' },
+        { 'nvim-lua/plenary.nvim' },
+      },
+      config = [[require('config.telescope')]]
     },
-    config = [[require('config.telescope')]]
-  }
-  use {'nvim-telescope/telescope-fzf-native.nvim', run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' }
-  use {
-    'nvim-telescope/telescope-cheat.nvim',
-    requires = { 'tami5/sql.nvim' },
-    config = [[require('telescope').load_extension('cheat')]],
-    after = 'telescope.nvim',
-  }
+    {
+      'nvim-telescope/telescope-fzf-native.nvim',
+      run = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    },
+    {
+      'nvim-telescope/telescope-cheat.nvim',
+      requires = { 'tami5/sql.nvim' },
+      config = [[require('telescope').load_extension('cheat')]],
+      after = 'telescope.nvim',
+    },
 
-  -- Lines
-  use { 'nvim-lualine/lualine.nvim', config = [[require('config.lualine')]] }
-  use { 'kyazdani42/nvim-tree.lua', config = [[require('config.tree')]], requires = { 'kyazdani42/nvim-web-devicons' } }
-  use { 'akinsho/bufferline.nvim', config = [[require('config.bufferline')]], requires = { 'kyazdani42/nvim-web-devicons' } }
-  use { 'moll/vim-bbye' }
+    -- Lines
+    { 'nvim-lualine/lualine.nvim', config = [[require('config.lualine')]] },
+    { 'kyazdani42/nvim-tree.lua', config = [[require('config.tree')]], requires = { 'kyazdani42/nvim-web-devicons' } },
+    { 'akinsho/bufferline.nvim', config = [[require('config.bufferline')]], requires = { 'kyazdani42/nvim-web-devicons' } },
+    'moll/vim-bbye',
 
-  -- Markdown
-  use { 'gabrielelana/vim-markdown', ft = 'markdown', config = [[require('config.markdown')]], }
+    -- Markdown
+    { 'gabrielelana/vim-markdown', ft = 'markdown', config = [[require('config.markdown')]] },
+  }
 end)
