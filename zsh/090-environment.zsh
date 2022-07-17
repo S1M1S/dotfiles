f4b73e4db308b201633b0c4bac4bdc9d5d634b97
Sam Ritchie <samm0s@hotmail.com>
2022-07-17 14:52:42 +1000

    zsh: accidentally left nvm out

--- zsh/090-environment.zsh
+++ zsh/090-environment.zsh
@@ -27,6 +27,10 @@ if [ "$TERM" = "xterm-256color" ]; then
   test -e "$DF_CONFIG/.iterm2/iterm2.zsh" && source "$DF_CONFIG/.iterm2/iterm2.zsh"
 fi
 
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 # start up cargo
 if [ -f "$HOME/.cargo/env" ]; then
   source "$HOME/.cargo/env"
